82008140ab0d2d6a82d5ed65c0f185c102eb6864
CodeDeveloper <nikitaskorobogatovvw@gmail.com>
2021-07-28 03:59:19 +0800

    Create all.php

---
+++ wp-content/cache/divispace/posts/19/all.php
@@ -0,0 +1,161 @@
+<?php DiviSpaceCache::mustBeRendering(); ?><div id="et-boc" class="et-boc">
+			
+		<div class="et-l et-l--post">
+			<div class="et_builder_inner_content et_pb_gutters3">
+		<script class="divi-rocket-added" type="text/javascript">
+				var et_animation_data = [{"class":"et_pb_text_0","style":"zoom","repeat":"once","duration":"1000ms","delay":"100ms","intensity":"10%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_text_1","style":"slideLeft","repeat":"once","duration":"1000ms","delay":"0ms","intensity":"10%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_row_1","style":"slideBottom","repeat":"once","duration":"1000ms","delay":"0ms","intensity":"2%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_blurb_0","style":"zoomLeft","repeat":"once","duration":"1000ms","delay":"0ms","intensity":"12%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_blurb_1","style":"zoomLeft","repeat":"once","duration":"1000ms","delay":"100ms","intensity":"12%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_blurb_2","style":"zoomLeft","repeat":"once","duration":"1000ms","delay":"200ms","intensity":"12%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_blurb_3","style":"zoomLeft","repeat":"once","duration":"1000ms","delay":"300ms","intensity":"12%","starting_opacity":"0%","speed_curve":"ease-in-out"},{"class":"et_pb_contact_form_0","style":"slideTop","repeat":"once","duration":"1000ms","delay":"0ms","intensity":"10%","starting_opacity":"0%","speed_curve":"ease-in-out"}];
+				</script><div class="et_pb_section et_pb_section_0 et_pb_with_background et_section_regular" >
+				
+				
+				
+				
+					<div class="et_pb_row et_pb_row_0">
+				<div class="et_pb_column et_pb_column_2_3 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough">
+				
+				
+				<div class="et_pb_module et_pb_text et_pb_text_0 et_animated  et_pb_text_align_left et_pb_bg_layout_light">
+				
+				
+				<div class="et_pb_text_inner"><h1>Contact Us</h1></div>
+			</div> <!-- .et_pb_text --><div class="et_pb_module et_pb_text et_pb_text_1 et_animated  et_pb_text_align_left et_pb_bg_layout_light">
+				
+				
+				<div class="et_pb_text_inner"><p><span>A Divine Design is the GTA’s #1 California, Plantation shutter, and roller blinds provider. We are the first shutter manufacturer in Canada to provide consumers with various California shutter styles. The Toronto Shutter is the world’s first curved California shutter. Made out of the Best 100% Canadian wood and vinyl, this shutter is one to look out for. </span><a class="open-widget">Contact Us Today</a><span> to book your </span><strong>Free In-Home Consultation</strong></p></div>
+			</div> <!-- .et_pb_text -->
+			</div> <!-- .et_pb_column --><div class="et_pb_column et_pb_column_1_3 et_pb_column_1  et_pb_css_mix_blend_mode_passthrough et-last-child et_pb_column_empty">
+				
+				
+				
+			</div> <!-- .et_pb_column -->
+				
+				
+			</div> <!-- .et_pb_row -->
+				
+				
+			</div> <!-- .et_pb_section --><div class="et_pb_section et_pb_section_1 et_pb_with_background et_section_regular" >
+				
+				
+				
+				
+					<div class="et_pb_row et_pb_row_1 et_animated">
+				<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough">
+				
+				
+				<div class="et_pb_module et_pb_blurb et_pb_blurb_0 et_animated  et_pb_text_align_left  et_pb_blurb_position_left et_pb_bg_layout_dark">
+				
+				
+				<div class="et_pb_blurb_content">
+					<div class="et_pb_main_blurb_image"><span class="et_pb_image_wrap"><span class="et-waypoint et_pb_animation_top et-pb-icon">&#xe081;</span></span></div>
+					<div class="et_pb_blurb_container">
+						<h4 class="et_pb_module_header"><span>Location</span></h4>
+						<div class="et_pb_blurb_description"><p>Toronto, ON</p></div>
+					</div>
+				</div> <!-- .et_pb_blurb_content -->
+			</div> <!-- .et_pb_blurb --><div class="et_pb_module et_pb_blurb et_pb_blurb_1 et_animated  et_pb_text_align_left  et_pb_blurb_position_left et_pb_bg_layout_dark">
+				
+				
+				<div class="et_pb_blurb_content">
+					<div class="et_pb_main_blurb_image"><span class="et_pb_image_wrap"><span class="et-waypoint et_pb_animation_top et-pb-icon">&#xe090;</span></span></div>
+					<div class="et_pb_blurb_container">
+						<h4 class="et_pb_module_header"><span>Phone</span></h4>
+						<div class="et_pb_blurb_description"><p>(416) 230-1043</p></div>
+					</div>
+				</div> <!-- .et_pb_blurb_content -->
+			</div> <!-- .et_pb_blurb --><div class="et_pb_module et_pb_blurb et_pb_blurb_2 et_animated  et_pb_text_align_left  et_pb_blurb_position_left et_pb_bg_layout_dark">
+				
+				
+				<div class="et_pb_blurb_content">
+					<div class="et_pb_main_blurb_image"><span class="et_pb_image_wrap"><span class="et-waypoint et_pb_animation_top et-pb-icon">&#xe076;</span></span></div>
+					<div class="et_pb_blurb_container">
+						<h4 class="et_pb_module_header"><span>Email</span></h4>
+						<div class="et_pb_blurb_description"><p>contact@adivinedesign.ca</p></div>
+					</div>
+				</div> <!-- .et_pb_blurb_content -->
+			</div> <!-- .et_pb_blurb --><div class="et_pb_module et_pb_blurb et_pb_blurb_3 et_animated  et_pb_text_align_left  et_pb_blurb_position_left et_pb_bg_layout_dark">
+				
+				
+				<div class="et_pb_blurb_content">
+					<div class="et_pb_main_blurb_image"><span class="et_pb_image_wrap"><span class="et-waypoint et_pb_animation_top et-pb-icon">&#xe06b;</span></span></div>
+					<div class="et_pb_blurb_container">
+						<h4 class="et_pb_module_header"><span>Open Hours</span></h4>
+						<div class="et_pb_blurb_description"><p><strong>Mon:</strong>9am &#8211; 6pm<br /> <strong>Tue:</strong> 9am &#8211; 6pm<br /> <strong>Wed:</strong> 9am &#8211; 6pm<br /> <strong>Thur:</strong> 9am &#8211; 6pm<br /> <strong>Fri:</strong> 9am &#8211; 6pm<br /> <strong>Sat:</strong> 10am &#8211; 3pm<br /> <strong>Sun:</strong> Closed</p></div>
+					</div>
+				</div> <!-- .et_pb_blurb_content -->
+			</div> <!-- .et_pb_blurb -->
+			</div> <!-- .et_pb_column --><div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child">
+				
+				
+				<?php ob_start(); ?><p class="et_pb_contact_field et_pb_contact_field_0 et_pb_contact_field_last" data-id="name" data-type="input">
+				
+				
+				<label for="et_pb_contact_name_0" class="et_pb_contact_form_label">Name</label>
+				<input type="text" id="et_pb_contact_name_0" class="input" value="" name="et_pb_contact_name_0" data-required_mark="required" data-field_type="input" data-original_id="name" placeholder="Name">
+			</p><p class="et_pb_contact_field et_pb_contact_field_1 et_pb_contact_field_last" data-id="email" data-type="email">
+				
+				
+				<label for="et_pb_contact_email_0" class="et_pb_contact_form_label">Email Address</label>
+				<input type="text" id="et_pb_contact_email_0" class="input" value="" name="et_pb_contact_email_0" data-required_mark="required" data-field_type="email" data-original_id="email" placeholder="Email Address">
+			</p><p class="et_pb_contact_field et_pb_contact_field_2 et_pb_contact_field_last" data-id="message" data-type="text">
+				
+				
+				<label for="et_pb_contact_message_0" class="et_pb_contact_form_label">Message</label>
+				<textarea name="et_pb_contact_message_0" id="et_pb_contact_message_0" class="et_pb_contact_message input" data-required_mark="required" data-field_type="text" data-original_id="message" placeholder="Message"></textarea>
+			</p><?php DiviSpaceCache::renderShortcode('et_pb_contact_form',array (
+  'title' => 'Send A Message',
+  'submit_button_text' => 'Send Message',
+  'module_id' => 'et_pb_contact_form_0',
+  '_builder_version' => '4.5.7',
+  'form_field_background_color' => 'rgba(255,255,255,0.12)',
+  'form_field_text_color' => '#ffffff',
+  'title_font' => 'Teko||||||||',
+  'title_text_color' => '#ffffff',
+  'title_font_size' => '40px',
+  'title_line_height' => '1.2em',
+  'form_field_font' => 'Lato||||||||',
+  'form_field_font_size' => '16px',
+  'custom_button' => 'on',
+  'button_text_color' => '#c10f1b',
+  'button_bg_color' => '#ffffff',
+  'button_border_width' => '8px',
+  'button_border_color' => '#ffffff',
+  'button_border_radius' => '0px',
+  'button_letter_spacing' => '4px',
+  'button_font' => 'Teko|||on|||||',
+  'button_icon' => '%%3%%',
+  'animation_style' => 'slide',
+  'animation_direction' => 'top',
+  'animation_intensity_slide' => '10%',
+  'hover_enabled' => '0',
+  'form_background_color' => 'rgba(255,255,255,0.12)',
+  'locked' => 'off',
+  'captcha_text_color' => '#ffffff',
+), 0, 0, true, ob_get_clean()); ?>
+			</div> <!-- .et_pb_column -->
+				
+				
+			</div> <!-- .et_pb_row -->
+				
+				
+			</div> <!-- .et_pb_section --><div class="et_pb_section et_pb_section_2 et_pb_fullwidth_section et_section_regular" >
+				
+				
+				
+				
+					<div class="et_pb_module et_pb_fullwidth_map_0 et_pb_map_container">
+				
+				
+				<div class="et_pb_map" data-center-lat="43.653226" data-center-lng="-79.3831843" data-zoom="10" data-mouse-wheel="off" data-mobile-dragging="on"></div>
+				<div class="et_pb_map_pin" data-lat="37.7749295" data-lng="-122.4194155" data-title="">
+				
+				
+			</div>
+			</div>
+				
+				
+			</div> <!-- .et_pb_section -->
+		</div><!-- .et_builder_inner_content -->
+	</div><!-- .et-l -->
+	
+			
+		</div><!-- #et-boc -->
+		<link rel='stylesheet' id='et-builder-googlefonts-1-css'  href='https://fonts.googleapis.com/css?family=Lato:100,100italic,300,300italic,regular,italic,700,700italic,900,900italic|Teko:300,regular,500,600,700&#038;subset=latin,latin-ext&#038;display=swap' type='text/css' media='all' />
