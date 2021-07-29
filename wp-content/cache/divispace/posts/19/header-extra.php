184a280218f07bbdb12c431837a3f55ecb89388b
CodeDeveloper <nikitaskorobogatovvw@gmail.com>
2021-07-30 03:59:02 +0800

    Create header-extra.php

---
+++ wp-content/cache/divispace/posts/19/header-extra.php
@@ -0,0 +1,64 @@
+<style class="divi-rocket-added">
+div.et_pb_section.et_pb_section_0 { background-image: linear-gradient(180deg,rgba(255,255,255,0.9) 0%,#ffffff 100% ), url(https://adivinedesign.ca/wp-content/uploads/2019/11/car-dealer-35.jpg) !important; }
+.et_pb_section_0.et_pb_section { padding-top: 0px; padding-right: 10%; padding-bottom: 0px; padding-left: 10%;margin-top: 0px; }
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_section_0.et_pb_section { padding-right: 30px; padding-left: 30px; }
+}
+.et_pb_row_0.et_pb_row { padding-top: 0px !important; padding-bottom: 0px !important; padding-top: 0; padding-bottom: 0px; }
+.et_pb_row_0, body #page-container .et-db #et-boc .et-l .et_pb_row_0.et_pb_row, body.et_pb_pagebuilder_layout.single #page-container #et-boc .et-l .et_pb_row_0.et_pb_row, body.et_pb_pagebuilder_layout.single.et_full_width_page #page-container #et-boc .et-l .et_pb_row_0.et_pb_row { width: 100%; max-width: 100%; }
+.et_pb_column_0 { background-image: linear-gradient(180deg,rgba(0,0,0,0.07) 0%,rgba(255,255,255,0) 100% ); padding-top: 10vw; padding-right: 40px; padding-bottom: 10vw; padding-left: 40px; }
+
+@media only screen and ( max-width: 767px ) {
+	.et_pb_column_0 { padding-right: 10px; padding-left: 10px; }
+}
+.et_pb_text_0 p { line-height: 1.8em; }
+.et_pb_text_0 { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 20px; line-height: 1.8em; margin-right: -10vw !important; margin-bottom: 0px !important; }
+.et_pb_text_0 h1 { font-family: 'Teko', Helvetica, Arial, Lucida, sans-serif; font-weight: 500; text-transform: uppercase;font-size: 9vw; color: #000000 !important; letter-spacing: 0.05em; line-height: 0.9em; text-shadow: 0em 0.18em 0.27em rgba(0,0,0,0.24); }
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_text_0 { margin-right: 0vw !important; }
+}
+
+@media only screen and ( max-width: 767px ) {
+	.et_pb_text_0 h1 { line-height: 1.2em; }
+	.et_pb_text_0 { margin-right: -5vw !important; }
+}
+.et_pb_text_1 p { line-height: 1.8em; }
+.et_pb_text_1 { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 20px; line-height: 1.8em; margin-right: -10vw !important; margin-left: 10vw !important; }
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_text_1 { margin-right: 0vw !important; margin-left: 0px !important; }
+}
+div.et_pb_section.et_pb_section_1 { background-image: linear-gradient(180deg,rgba(27,31,37,0.3) 0%,#1b1f25 100% ), url(https://adivinedesign.ca/wp-content/uploads/2019/11/asset.jpeg) !important; }
+.et_pb_section_1.et_pb_section { padding-top: 0px; padding-bottom: 10vw; }
+.et_pb_section_1 { box-shadow: inset 0px 100px 0px 0px #ffffff ; }
+.et_pb_row_1.et_pb_row { padding-top: 0px !important; padding-bottom: 0px !important; padding-top: 0px; padding-bottom: 0px; }
+.et_pb_column_2 { background-image: linear-gradient(190deg,#c10f1b 85%,rgba(255,255,255,0) 0% ); padding-top: 60px; padding-right: 5%; padding-bottom: 180px; padding-left: 5%; }
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_column_2 { padding-right: 10%; padding-left: 10%; }
+}
+.et_pb_blurb_0.et_pb_blurb p { line-height: 1.8em; }
+.et_pb_blurb_0.et_pb_blurb .et_pb_module_header, .et_pb_blurb_0.et_pb_blurb .et_pb_module_header a { font-family: 'Teko', Helvetica, Arial, Lucida, sans-serif; font-weight: 500;font-size: 28px; line-height: 1.2em; }
+.et_pb_blurb_0.et_pb_blurb { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; line-height: 1.8em; }
+.et_pb_blurb_0 .et-pb-icon { font-size: 28px; color: #ffffff; }
+.et_pb_blurb_1.et_pb_blurb p { line-height: 1.8em; }
+.et_pb_blurb_1.et_pb_blurb .et_pb_module_header, .et_pb_blurb_1.et_pb_blurb .et_pb_module_header a { font-family: 'Teko', Helvetica, Arial, Lucida, sans-serif; font-weight: 500;font-size: 28px; line-height: 1.2em; }
+.et_pb_blurb_1.et_pb_blurb { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; line-height: 1.8em; }
+.et_pb_blurb_1 .et-pb-icon { font-size: 28px; color: #ffffff; }
+.et_pb_blurb_2.et_pb_blurb p { line-height: 1.8em; }
+.et_pb_blurb_2.et_pb_blurb .et_pb_module_header, .et_pb_blurb_2.et_pb_blurb .et_pb_module_header a { font-family: 'Teko', Helvetica, Arial, Lucida, sans-serif; font-weight: 500;font-size: 28px; line-height: 1.2em; }
+.et_pb_blurb_2.et_pb_blurb { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; line-height: 1.8em; }
+.et_pb_blurb_2 .et-pb-icon { font-size: 28px; color: #ffffff; }
+.et_pb_blurb_3.et_pb_blurb p { line-height: 1.8em; }
+.et_pb_blurb_3.et_pb_blurb .et_pb_module_header, .et_pb_blurb_3.et_pb_blurb .et_pb_module_header a { font-family: 'Teko', Helvetica, Arial, Lucida, sans-serif; font-weight: 500;font-size: 28px; line-height: 1.2em; }
+.et_pb_blurb_3.et_pb_blurb { font-family: 'Lato', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; line-height: 1.8em; }
+.et_pb_blurb_3 .et-pb-icon { font-size: 28px; color: #ffffff; }
+.et_pb_column_3 { padding-top: 180px; }
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_column_3 { padding-top: 0px; }
+}
+.et_pb_fullwidth_map_0 { filter: saturate(0%);backface-visibility:hidden; }
+html:not(.et_fb_edit_enabled) #et-fb-app .et_pb_fullwidth_map_0:hover { filter: saturate(0%); }</style>
